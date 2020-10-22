c6bd8f7c140b8979d9baf8dc614f13637e67ddb8
jamescadigan97 <64744915+jamescadigan97@users.noreply.github.com>
2020-10-21 20:33:45 -0400

    Leaflet 1

---
+++ Leaflet1/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiamFtZXNkY2FkaWdhbjIyIiwiYSI6ImNrZm94bWdsejA2cDUydGxkbDdtd3Z5aDcifQ.hs5gxmnCxgb2lMIDM46ITg";
---
+++ Leaflet2/static/js/logic.js
@@ -0,0 +1,169 @@
+// //Create base layer
+
+// var myMap = L.map("map", {
+//   center: [39.82, -98.58],
+//   zoom: 5
+// });
+
+// L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//   tileSize: 512,
+//   zoomOffset: -1,
+//   id: "mapbox/streets-v11",
+//   accessToken: API_KEY
+// }).addTo(myMap);
+
+// var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//   maxZoom: 18,
+//   id: "outdoors-v9",
+//   accessToken: API_KEY
+// });
+
+// var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//   maxZoom: 18,
+//   id: "satellite-streets-v9",
+//   accessToken: API_KEY
+// });
+
+// var baseMaps = {
+//   Outdoors: outdoors,
+//   Satellite: satellite
+// };
+
+// L.control.layers(baseMaps).addTo(myMap);
+
+// //Read JSON file and run function to create markers
+
+// url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+// d3.json(url,create_circles)
+
+// //Function to create make
+// function create_circles(data){
+
+//   //Show Objects in console
+//   console.log("Show Data")
+//   console.log(data)
+//   console.log("Features")
+//   features = data.features
+//   console.log(features)
+   
+//   //Loop through Data
+//   for (var i = 0; i < features.length; i++) {
+//     var response = features[i]
+//     coordinate = response.geometry.coordinates
+
+//     //Write if statement to decide the color
+//     var colors = "";
+//       if (response.properties.mag > 4) {
+//         colors = "red";
+//       }
+//       else if (response.properties.mag > 3 ){
+//         colors = "yellow";
+//       }
+//       else {
+//         colors = "green";
+//       }
+
+//     //Add markers
+//     circle = L.circle([coordinate[1],coordinate[0]], {
+
+//         //Change color based on magnitude
+//         color: colors,
+//         fillColor: colors,
+//         fillOpacity: 0.5,
+
+//         //change radius base on depth
+//         radius: coordinate[2] * 500
+//     //Add popup
+//     }).bindPopup("<h2>" + response.properties.place + "</h2> <hr> <h3>Magnitude: " + response.properties.mag + "</h3>").addTo(myMap);  
+//    }  
+//   }
+
+  //Create base layer
+
+var myMap = L.map("map", {
+  center: [39.82, -98.58],
+  zoom: 5
+});
+
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "outdoors-v9",
+  accessToken: API_KEY
+});
+
+var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "satellite-streets-v9",
+  accessToken: API_KEY
+});
+
+var baseMaps = {
+  Outdoors: outdoors,
+  Satellite: satellite
+};
+
+L.control.layers(baseMaps).addTo(myMap);
+
+//Read JSON file and run function to create markers
+
+url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+d3.json(url,create_circles)
+
+//Function to create make
+function create_circles(data){
+
+  //Show Objects in console
+  console.log("Show Data")
+  console.log(data)
+  console.log("Features")
+  features = data.features
+  console.log(features)
+   
+  //Loop through Data
+  for (var i = 0; i < features.length; i++) {
+    var response = features[i]
+    coordinate = response.geometry.coordinates
+
+    //Write if statement to decide the color
+    var colors = "";
+      if (response.properties.mag > 4) {
+        colors = "red";
+      }
+      else if (response.properties.mag > 3 ){
+        colors = "yellow";
+      }
+      else {
+        colors = "green";
+      }
+
+    //Add markers
+    circle = L.circle([coordinate[1],coordinate[0]], {
+
+        //Change color based on magnitude
+        color: colors,
+        fillColor: colors,
+        fillOpacity: 0.5,
+
+        //change radius base on depth
+        radius: coordinate[2] * 500
+    //Add popup
+    }).bindPopup("<h2>" + response.properties.place + "</h2> <hr> <h3>Magnitude: " + response.properties.mag + "</h3>").addTo(myMap);  
+   }  
+  }
+
+  d3.json("plates.json",function(data){
+    console.log(data)
+  });
